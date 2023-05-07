854129635f914b303823e85bd971a81a1921b88b
Raul Neto <rjbnetto@gmail.com>
2023-05-07 14:29:53 -0300

    Update Jo-Ken-Po.py
    Atualização e Finalização do Jo-Ken-Po

--- Jo-Ken-Po.py
+++ Jo-Ken-Po.py
@@ -1,6 +1,6 @@
 import random
 
-#Escolhas
+#Escolhas possíveis
 opcoes = ["pedra", "papel", "tesoura"]
 
 #Jogadores
@@ -10,23 +10,38 @@ jogada_computador = random.choice(opcoes)
 print("Você jogou " + jogada_jogador)
 print("Computador jogou " + jogada_computador)
 
-#Resultados
+#Resultados possíveis
 if jogada_jogador not in opcoes:
     print("Jogada inválida.")
+    print("Desenvolvido por Raul Neto // FATEC SJC // Banco de Dados // 1° Semestre no ano de 2023")
 elif jogada_jogador == jogada_computador:
     print("Empate!")
+    print("Desenvolvido por Raul Neto // FATEC SJC // Banco de Dados // 1° Semestre no ano de 2023")
 elif jogada_jogador == "pedra":
     if jogada_computador == "papel":
         print("Você perdeu!")
+        print("Desenvolvido por Raul Neto // FATEC SJC // Banco de Dados // 1° Semestre no ano de 2023")
     else:
         print("Você venceu!")
+        print("Desenvolvido por Raul Neto // FATEC SJC // Banco de Dados // 1° Semestre no ano de 2023")
 elif jogada_jogador == "papel":
     if jogada_computador == "tesoura":
         print("Você perdeu!")
+        print("Desenvolvido por Raul Neto // FATEC SJC // Banco de Dados // 1° Semestre no ano de 2023")
     else:
         print("Você venceu!")
+        print("Desenvolvido por Raul Neto // FATEC SJC // Banco de Dados // 1° Semestre no ano de 2023")
 elif jogada_jogador == "tesoura":
     if jogada_computador == "pedra":
         print("Você perdeu!")
+        print("Desenvolvido por Raul Neto // FATEC SJC // Banco de Dados // 1° Semestre no ano de 2023")
     else:
         print("Você venceu!")
+        print("Desenvolvido por Raul Neto // FATEC SJC // Banco de Dados // 1° Semestre no ano de 2023")
+
+        # '''Em relação a Matemática Discreta, podemos visualizar em um simples jogo de Jo-Ken-Po. 
+        # Relações Binárias a partir do possíveis resultados:
+        # Conjuto = (Pedra, Papel e Tesoura)
+        # (Pedra, Papel); (Pedra, Tesoura); (Pedra, Pedra)
+        # (Papel, Papel); (Papel, Tesoura); (Papel, Pedra)
+        # (Tesoura, Papel); (Tesoura, Tesoura); (Tesoura, Pedra)
